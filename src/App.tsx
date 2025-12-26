4700ce33f1f0fea785bb8836f774ed7550812389
shubhram krishna <shubhram.krishna@ibm.com>
2025-12-26 21:01:06 +0530

    suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,62 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./hoc/layout/Layout";
+import Home from "./containers/home/Home";
+import ProductDetail from "./containers/product/product-detail/ProductDetail";
+import ErrorPage from "./containers/error/ErrorPage";
+import ProductList from "./containers/product/product-list/ProductList";
+import ProtectedRoute from "./hoc/protected-route/ProtectedRoute";
+import UnAuthorized from "./containers/error/UnAuthorized";
+import React, { Suspense } from "react";
+import ProductLoader from "./containers/product/product-detail/ProductLoader";
 
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Layout from './hoc/layout/Layout';
-import Home from './containers/home/Home';
-import ProductDetail from './containers/product/product-detail/ProductDetail';
-import ErrorPage from './containers/error/ErrorPage';
-import ProductList from './containers/product/product-list/ProductList';
-import ProtectedRoute from './hoc/protected-route/ProtectedRoute';
-import UnAuthorized from './containers/error/UnAuthorized';
-import React, { Suspense } from 'react';
-
-const user =  { name: "John Doe" };
+const user = { name: "John Doe" };
 function App() {
-
   //const Payment = React.lazy(() => import("./containers/order/payment/Payment"));
   // Simulate a delay for loading the Payment component to show Suspense in action
   const Payment = React.lazy(
     () =>
       new Promise((resolve) => {
-        setTimeout(() => resolve(import("./containers/order/payment/Payment")), 2000);
+        setTimeout(
+          () => resolve(import("./containers/order/payment/Payment")),
+          2000
+        );
       })
   );
-
-
 
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <Routes>
-          <Route element={<Layout />}>
+        <Route element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="products" element={<ProductList />} />
-          <Route path="products/:id" element={<ProductDetail />} />
+
+          <Route
+            path="products/:id"
+            element={
+              <Suspense fallback={<ProductLoader />}>
+                <ProductDetail />
+              </Suspense>
+            }
+          />
+          
           <Route
             path="payment"
             element={
               <ProtectedRoute user={user} redirectPath="/unauthorized">
                 <Suspense fallback={<p>Loading...</p>}>
-                      <Payment />
+                  <Payment />
                 </Suspense>
               </ProtectedRoute>
             }
           />
+
           <Route path="unauthorized" element={<UnAuthorized />} />
           <Route path="/*" element={<ErrorPage />} />
         </Route>
-      </Routes>  
+      </Routes>
     </Suspense>
   );
 }
 
-export default App
+export default App;
--- src/containers/product/product-detail/ProductDetail.tsx
+++ src/containers/product/product-detail/ProductDetail.tsx
@@ -5,6 +5,7 @@ import ProductQuantity from "../product-card/ProductQuantity";
 import { useSelector } from "react-redux";
 import type { RootState } from "../../../store";
 import type { ProductItem } from "../../../models/product-item.model";
+import ProductLoader from "./ProductLoader";
 
 const ProductDetail: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -22,18 +23,7 @@ const ProductDetail: React.FC = () => {
 
   if (loading) {
     return (
-      <section className="py-12 bg-gray-50">
-        <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-10 animate-pulse">
-          <div className="bg-gray-200 h-96 rounded-xl" />
-          <div>
-            <div className="h-6 bg-gray-200 rounded w-3/4 mb-4" />
-            <div className="h-6 bg-gray-200 rounded w-1/4 mb-6" />
-            <div className="h-4 bg-gray-200 rounded w-full mb-2" />
-            <div className="h-4 bg-gray-200 rounded w-5/6 mb-6" />
-            <div className="h-10 bg-gray-200 rounded w-1/2" />
-          </div>
-        </div>
-      </section>
+     <ProductLoader />
     );
   }
 
